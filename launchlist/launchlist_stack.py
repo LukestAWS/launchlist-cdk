2b512e09c168c76dbfec935e73a59524daecf161
Lukest Mensah <mlukest@gmail.com>
2025-12-30 03:43:22 +0000

    fix: use OriginAccessControl + private bucket for CloudFront (403 fix)

--- launchlist/launchlist_stack.py
+++ launchlist/launchlist_stack.py
@@ -14,24 +14,44 @@ class LaunchlistStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Private S3 bucket
         bucket = s3.Bucket(self, "SiteBucket",
-            block_public_access=s3.BlockPublicAccess(block_public_policy=False),  # fix public policy block
-            public_read_access=True,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  # private
             website_index_document="index.html",
             auto_delete_objects=True,
             removal_policy=RemovalPolicy.DESTROY,
         )
 
+        # Origin Access Control (modern replacement for OAI)
+        oac = cloudfront.OriginAccessControl(self, "OAC",
+            origin_access_control_name="LaunchlistOAC",
+            signing_behavior=cloudfront.SigningBehavior.SIGV4_ALWAYS,
+        )
+
+        # CloudFront distribution with OAC
+        distribution = cloudfront.Distribution(self, "SiteDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3BucketOrigin.with_origin_access_control(bucket, origin_access_control=oac),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
+        )
+
+        # Bucket policy to allow CloudFront OAC
+        bucket.add_to_resource_policy(
+            s3.PolicyStatement(
+                actions=["s3:GetObject"],
+                effect=s3.Effect.ALLOW,
+                principals=[cloudfront.DistributionPrincipal(distribution)],
+                resources=[bucket.arn_for_objects("*")],
+            )
+        )
+
+        # Deploy assets
         s3deploy.BucketDeployment(self, "DeploySite",
             sources=[s3deploy.Source.asset("./assets")],
             destination_bucket=bucket,
-        )
-
-        distribution = cloudfront.Distribution(self, "SiteDistribution",
-            default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(bucket),
-                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            ),
+            distribution=distribution,
+            distribution_paths=["/*"],
         )
 
         CfnOutput(self, "SiteURL", value=distribution.distribution_domain_name)
